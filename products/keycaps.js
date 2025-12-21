349f840dfd52c29cb2be12a2613050a9e5b3e27f
Glsswrks <nara04542@gmail.com>
2025-12-22 06:56:24 +0700

    Add files via upload

---
+++ products/keycaps.js
@@ -0,0 +1,84 @@
+export const keycaps = [
+{
+      id: "cyberpunk-keycap",
+      title: "Side-Print Cyberpunk Keycap",
+      short:
+        "Cyberpunk Themed Keycap compatible with all keyboards that using Cherry Profile",
+      price: 25,
+      layout: "Cherry Profile",
+      available: false,
+      no_info: true,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://titannation.store/cdn/shop/files/keycaps_set_PBT.jpg?v=1728900168",
+        "https://titannation.store/cdn/shop/files/172_PC.jpg?v=1727684963&width=1946",
+      ],
+      specs: [
+        "Cherry Profile",
+        "Material: PC",
+        "Support Full Keyboard Layouts",
+        "172 Keys",
+      ],
+      options: null,
+    },
+    {
+      id: "cyberpunk-keycap-74",
+      title: "Custom Cyberpunk Keycap (74 Keys)",
+      short:
+        "Custom Cyberpunk Themed Keycap compatible with all keyboards that using Original Profile",
+      price: 15,
+      layout: "Original Profile",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://media.discordapp.net/attachments/1384747917063225354/1451608935948878058/tb_image_share_1766160794768.png?ex=69481d2b&is=6946cbab&hm=be9a2d7725e4e655868a009e4e58c4ad9ef651e99111ed977e4b046dcb656c15&=&format=webp&quality=lossless&width=693&height=693",
+      ],
+      specs: [
+        "Original Profile",
+        "Material: PBT",
+        "Support Full Keyboard 61/68 Layouts",
+        "74 Keys",
+      ],
+      options: [
+        {
+          name: "Original Cyberpunk Theme",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451608935948878058/tb_image_share_1766160794768.png?ex=69481d2b&is=6946cbab&hm=be9a2d7725e4e655868a009e4e58c4ad9ef651e99111ed977e4b046dcb656c15&=&format=webp&quality=lossless&width=693&height=693",
+        },
+        {
+          name: "Dark-Red Cyberpunk Theme",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451608936343011369/tb_image_share_1766160798018.png?ex=69481d2c&is=6946cbac&hm=17a91f76363c8c9dc148091d4ae9e3efbd0c4b4c05ab47fd2322849c5fc70003&=&format=webp&quality=lossless&width=693&height=693",
+        },
+      ],
+    },
+    {
+      id: "yuki-aim-keycap",
+      title: "Yuki Aim Keycap",
+      short:
+        "Yuki Aim Keycap compatible with all keyboards that using Original - Cherry Profile",
+      price: 15,
+      layout: "Cherry - Original Profile",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://media.discordapp.net/attachments/1384747917063225354/1452133712430104698/tb_image_share_1766285986918.png?ex=6948b468&is=694762e8&hm=58217e4dd3c08a77cf180263296c037ba02ce0aa056132c470f3f109892172eb&=&format=webp&quality=lossless&width=693&height=693",
+        "https://media.discordapp.net/attachments/1384747917063225354/1452133711943569559/tb_image_share_1766285992734.png?ex=6948b468&is=694762e8&hm=53a7981d6095e797ae5640bdc8f02abbc0cd17cab85a739767c1b44f99e6c794&=&format=webp&quality=lossless&width=693&height=693",
+        "https://media.discordapp.net/attachments/1384747917063225354/1452133712895545407/tb_image_share_1766285996468.png?ex=6948b468&is=694762e8&hm=e8050ff36a87fcfca0e696f38a34bb4430f98310eb4fca62ac2ace04d4e6c8c9&=&format=webp&quality=lossless&width=693&height=693",
+      ],
+      specs: [
+        "Cherry Original Height Profile",
+        "Material: PBT Thermal transfer",
+        "Support 61/68 Layout Keyboards",
+        "104 Keys",
+      ],
+      options: null,
+    },
+];
---
+++ products/mousepads.js
@@ -0,0 +1,163 @@
+export const mousepads = [
+     {
+      id: "pulsar-yui-edition",
+      title: "Pulsar Yui Edition Glass Mousepad (Replica)",
+      short:
+        "490x430mm Tempered Glass Mousepad - XXL Size (Aimerz Yui Edition)",
+      price: 65,
+      layout: "490x430",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://www.pulsar.gg/cdn/shop/files/Pulsar-Superglide-Glass-mousepad_XXL_Aimerz_Yui_main.jpg?v=1752193016",
+      ],
+      specs: [
+        "Tempered Glass Surface",
+        "Size: 490x430mm",
+        "Coating + Full Coverage Primer",
+      ],
+      options: null,
+    },
+    {
+      id: "pulsar-ayane-edition",
+      title: "Pulsar Ayane Edition Glass Mousepad (Replica)",
+      short: "490x430mm Tempered Glass Mousepad - XXL Size (Ayane Edition)",
+      price: 50,
+      layout: "490x430",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://www.pulsar.gg/cdn/shop/files/Pulsar_Superglide_Glass_mousepad_XXL_Aimerz_Ayane_main.jpg?v=1712547879",
+      ],
+      specs: [
+        "Tempered Glass Surface",
+        "Size: 490x430mm",
+        "Full Coverage Primer",
+      ],
+      options: null,
+    },
+    {
+      id: "pulsar-aimerz-edition",
+      title: "Pulsar Aimerz Edition Glass Mousepad (Replica)",
+      short: "490x430mm Tempered Glass Mousepad - XXL Size (Aimerz Edition)",
+      price: 50,
+      layout: "490x430",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://www.pulsar.gg/cdn/shop/files/Pulsar_Superglide_Glass_mousepad_XXL_Aimerz_main.jpg?v=1701254311",
+      ],
+      specs: [
+        "Tempered Glass Surface",
+        "Size: 490x430mm",
+        "Full Coverage Primer",
+      ],
+      options: null,
+    },
+    {
+      id: "pulsar-koharu-edition",
+      title: "Pulsar Koharu Edition Glass Mousepad (Replica)",
+      short: "490x430mm Tempered Glass Mousepad - XXL Size (Koharu Edition)",
+      price: 50,
+      layout: "490x430",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://www.pulsar.gg/cdn/shop/files/Pulsar_Superglide_Glass_mousepad_XXL_Aimerz_Koharu_main.jpg?v=1729639565",
+      ],
+      specs: [
+        "Tempered Glass Surface",
+        "Size: 490x430mm",
+        "Full Coverage Primer",
+      ],
+      options: null,
+    },
+    {
+      id: "titan-gaming-gear-mousepad",
+      title: "Titan Gaming Gear XSoft Standard",
+      short: "Premium XSoft Fabric Mousepad - Standard Size (Titan Nation)",
+      price: 15,
+      layout: "450x400",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://media.discordapp.net/attachments/1384747917063225354/1451902967295770695/tb_image_share_1766230995951.png?ex=6947dd82&is=69468c02&hm=cec800f002cb830346ea14edafd7f9aea4d301e82941009ba133ebe660160c48&=&format=webp&quality=lossless&width=693&height=693",
+        "https://media.discordapp.net/attachments/1384747917063225354/1451964001012547716/tb_image_share_1766245554292.png?ex=6948165a&is=6946c4da&hm=1858631ec46db3c378be77bb1c35dbcddd94f5201ab636de60ffb663b227cfb3&=&format=webp&quality=lossless&width=693&height=693",
+        "https://media.discordapp.net/attachments/1384747917063225354/1451964001457275072/tb_image_share_1766245559878.png?ex=6948165a&is=6946c4da&hm=7b4f1372a1fab7b2c052ff9cfbee484f864fb0396153a9d3038b3f86873630ca&=&format=webp&quality=lossless&width=693&height=693",
+        "https://media.discordapp.net/attachments/1384747917063225354/1451964001914192096/tb_image_share_1766245564112.png?ex=6948165a&is=6946c4da&hm=87fde99c62a283f01b76e1e1b1234de180d2ba920b92c82519641ecd79bbf01d&=&format=webp&quality=lossless&width=693&height=693",
+        "https://media.discordapp.net/attachments/1384747917063225354/1451964002346471618/tb_image_share_1766245569900.png?ex=6948165a&is=6946c4da&hm=406f21197b56616235c9bdaa3eec1ee0452770ffa6c1a611319f1e138a519f78&=&format=webp&quality=lossless&width=693&height=693",
+      ],
+      specs: ["Thickness: 5mm", "Size: 450x400mm"],
+      options: [
+        {
+          name: "Orange Edition",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451902967295770695/tb_image_share_1766230995951.png?ex=6947dd82&is=69468c02&hm=cec800f002cb830346ea14edafd7f9aea4d301e82941009ba133ebe660160c48&=&format=webp&quality=lossless&width=693&height=693",
+        },
+        {
+          name: "Black Edition",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451902967715467325/tb_image_share_1766230998852.png?ex=6947dd82&is=69468c02&hm=ce142a952de8e3da9d54fa4d54b936a971e7e4dd01f6d1d9fdc717649d228b85&=&format=webp&quality=lossless&width=693&height=693",
+        },
+        {
+          name: "Red Edition",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451902966914355241/tb_image_share_1766230992724.png?ex=6947dd82&is=69468c02&hm=ce06ad1e3bae737ab456b8e076613f3c2952c737375c783ece4475fa1ed05ba6&=&format=webp&quality=lossless&width=693&height=693",
+        },
+        {
+          name: "Blue Edition",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451902966507376737/tb_image_share_1766230989688.png?ex=6947dd82&is=69468c02&hm=58b2cae8119e22f9260f0dda2826900d2b3a743eb539324e979b4bc32deeb649&=&format=webp&quality=lossless&width=693&height=693",
+        },
+      ],
+    },
+    {
+      id: "clone-artisan-mousepad",
+      title: "Artisan FX (Clone)",
+      short: "Custom Artisan Mousepad Clone - 3mm Thickness",
+      price: 9,
+      layout: "450x400",
+      no_info: true,
+      available: false,
+      isNew: false,
+      lowStock: false,
+      images: [
+        "https://media.discordapp.net/attachments/1384747917063225354/1451902451786449070/tb_image_share_1766230886978.png?ex=6947dd07&is=69468b87&hm=f150f349e6d653a4de20d8c065953420be03d25016e60669a51fe6f6f26c6707&=&format=webp&quality=lossless&width=693&height=693",
+      ],
+      specs: ["Size: 450x400mm", "Thickness: 3mm"],
+      options: [
+        {
+          name: "Red Edition",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451902451786449070/tb_image_share_1766230886978.png?ex=6947dd07&is=69468b87&hm=f150f349e6d653a4de20d8c065953420be03d25016e60669a51fe6f6f26c6707&=&format=webp&quality=lossless&width=693&height=693",
+        },
+        {
+          name: "Orange Edition",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451902452436697119/tb_image_share_1766230894253.png?ex=6947dd07&is=69468b87&hm=1721ff5db8ded6af5bc9b3a0c306a6af26d20ad95b465003ada635270e598ec3&=&format=webp&quality=lossless&width=693&height=693",
+        },
+        {
+          name: "Black Edition",
+          available: false,
+          image:
+            "https://media.discordapp.net/attachments/1384747917063225354/1451902452851802354/tb_image_share_1766230881188.png?ex=6947dd07&is=69468b87&hm=30ad306b3f0793721980ff0e1f7e7d15948d2cf422f49ad8b20e80d36d30488d&=&format=webp&quality=lossless&width=693&height=693",
+        },
+      ],
+    }
+];
