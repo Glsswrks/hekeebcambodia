d2f9b1c8520f0e40698a8dd4e58639e62315944d
Glsswrks <nara04542@gmail.com>
2025-12-26 10:20:53 +0700

    Add files via upload

--- products/keycaps.js
+++ products/keycaps.js
@@ -1,84 +1,87 @@
 export const keycaps = [
-{
-      id: "cyberpunk-keycap",
-      title: "Side-Print Cyberpunk Keycap",
-      short:
-        "Cyberpunk Themed Keycap compatible with all keyboards that using Cherry Profile",
-      price: 25,
-      layout: "Cherry Profile",
-      available: false,
-      no_info: true,
-      isNew: false,
-      lowStock: false,
-      images: [
-        "https://titannation.store/cdn/shop/files/keycaps_set_PBT.jpg?v=1728900168",
-        "https://titannation.store/cdn/shop/files/172_PC.jpg?v=1727684963&width=1946",
-      ],
-      specs: [
-        "Cherry Profile",
-        "Material: PC",
-        "Support Full Keyboard Layouts",
-        "172 Keys",
-      ],
-      options: null,
-    },
-    {
-      id: "cyberpunk-keycap-74",
-      title: "Custom Cyberpunk Keycap (74 Keys)",
-      short:
-        "Custom Cyberpunk Themed Keycap compatible with all keyboards that using Original Profile",
-      price: 15,
-      layout: "Original Profile",
-      no_info: true,
-      available: false,
-      isNew: false,
-      lowStock: false,
-      images: [
-        "https://media.discordapp.net/attachments/1384747917063225354/1451608935948878058/tb_image_share_1766160794768.png?ex=69496eab&is=69481d2b&hm=93f8396819c7f774ebfcc5e310e78107c3e7f8d6ece52e2aea234d4cd5da789d&=&format=webp&quality=lossless&width=693&height=693",
-      ],
-      specs: [
-        "Original Profile",
-        "Material: PBT",
-        "Support Full Keyboard 61/68 Layouts",
-        "74 Keys",
-      ],
-      options: [
-        {
-          name: "Original Cyberpunk Theme",
-          available: false,
-          image:
-            "https://media.discordapp.net/attachments/1384747917063225354/1451608935948878058/tb_image_share_1766160794768.png?ex=69496eab&is=69481d2b&hm=93f8396819c7f774ebfcc5e310e78107c3e7f8d6ece52e2aea234d4cd5da789d&=&format=webp&quality=lossless&width=693&height=693",
-        },
-        {
-          name: "Dark-Red Cyberpunk Theme",
-          available: false,
-          image:
-            "https://media.discordapp.net/attachments/1384747917063225354/1451608936343011369/tb_image_share_1766160798018.png?ex=69496eac&is=69481d2c&hm=5b97d9501cfc3f35967b303f8c7c42af915848c8758ac86b160067207130f95c&=&format=webp&quality=lossless&width=693&height=693",
-        },
-      ],
-    },
-    {
-      id: "yuki-aim-keycap",
-      title: "Yuki Aim Keycap",
-      short:
-        "Yuki Aim Keycap compatible with all keyboards that using Original - Cherry Profile",
-      price: 15,
-      layout: "Cherry - Original Profile",
-      no_info: true,
-      available: false,
-      isNew: false,
-      lowStock: false,
-      images: [
-        "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285986918.jpg",
-        "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285992734.jpg",
-        "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285996468.jpg"
-       ],
-      specs: [
-        "Cherry Original Height Profile",
-        "Material: PBT Thermal transfer",
-        "Support 61/68 Layout Keyboards",
-        "104 Keys",
-      ],
-      options: null,
-    },
+  {
+    id: "cyberpunk-keycap",
+    title: "Side-Print Cyberpunk Keycap",
+    short:
+      "Cyberpunk Themed Keycap compatible with all keyboards that using Cherry Profile",
+    price: 25,
+    layout: "Cherry Profile",
+    available: false,
+    no_info: true,
+    isNew: false,
+    lowStock: false,
+    images: [
+      "https://titannation.store/cdn/shop/files/keycaps_set_PBT.jpg?v=1728900168",
+      "https://titannation.store/cdn/shop/files/172_PC.jpg?v=1727684963&width=1946",
+    ],
+    category: "keycaps",
+    specs: [
+      "Cherry Profile",
+      "Material: PC",
+      "Support Full Keyboard Layouts",
+      "172 Keys",
+    ],
+    options: null,
+  },
+  {
+    id: "cyberpunk-keycap-74",
+    title: "Custom Cyberpunk Keycap (74 Keys)",
+    short:
+      "Custom Cyberpunk Themed Keycap compatible with all keyboards that using Original Profile",
+    price: 15,
+    layout: "Original Profile",
+    no_info: true,
+    available: false,
+    isNew: false,
+    lowStock: false,
+    images: [
+      "https://media.discordapp.net/attachments/1384747917063225354/1451608935948878058/tb_image_share_1766160794768.png?ex=69496eab&is=69481d2b&hm=93f8396819c7f774ebfcc5e310e78107c3e7f8d6ece52e2aea234d4cd5da789d&=&format=webp&quality=lossless&width=693&height=693",
+    ],
+    category: "keycaps",
+    specs: [
+      "Original Profile",
+      "Material: PBT",
+      "Support Full Keyboard 61/68 Layouts",
+      "74 Keys",
+    ],
+    options: [
+      {
+        name: "Original Cyberpunk Theme",
+        available: false,
+        image:
+          "https://media.discordapp.net/attachments/1384747917063225354/1451608935948878058/tb_image_share_1766160794768.png?ex=69496eab&is=69481d2b&hm=93f8396819c7f774ebfcc5e310e78107c3e7f8d6ece52e2aea234d4cd5da789d&=&format=webp&quality=lossless&width=693&height=693",
+      },
+      {
+        name: "Dark-Red Cyberpunk Theme",
+        available: false,
+        image:
+          "https://media.discordapp.net/attachments/1384747917063225354/1451608936343011369/tb_image_share_1766160798018.png?ex=69496eac&is=69481d2c&hm=5b97d9501cfc3f35967b303f8c7c42af915848c8758ac86b160067207130f95c&=&format=webp&quality=lossless&width=693&height=693",
+      },
+    ],
+  },
+  {
+    id: "yuki-aim-keycap",
+    title: "Yuki Aim Keycap",
+    short:
+      "Yuki Aim Keycap compatible with all keyboards that using Original - Cherry Profile",
+    price: 15,
+    layout: "Cherry - Original Profile",
+    no_info: true,
+    available: false,
+    isNew: false,
+    lowStock: false,
+    images: [
+      "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285986918.jpg",
+      "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285992734.jpg",
+      "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285996468.jpg",
+    ],
+    category: "keycaps",
+    specs: [
+      "Cherry Original Height Profile",
+      "Material: PBT Thermal transfer",
+      "Support 61/68 Layout Keyboards",
+      "104 Keys",
+    ],
+    options: null,
+  },
 ];
