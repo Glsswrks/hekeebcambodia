9bc850e0745854b675fd5e704abee57d8eb690a0
Glsswrks <nara04542@gmail.com>
2026-01-03 21:27:16 +0700

    Add files via upload

---
+++ keycaps.js
@@ -0,0 +1,51 @@
+export const keycaps = [
+  {
+    id: "cyberpunk-keycap",
+    title: "Side-Print Cyberpunk Keycap",
+    short:
+      "Cyberpunk Themed Keycap compatible with all keyboards that using Cherry Profile",
+    price: 25,
+    layout: "Cherry Profile",
+    available: false,
+    no_info: true,
+    isNew: false,
+    lowStock: false,
+    images: [
+      "https://titannation.store/cdn/shop/files/keycaps_set_PBT.jpg?v=1728900168",
+      "https://titannation.store/cdn/shop/files/172_PC.jpg?v=1727684963&width=1946",
+    ],
+    category: "keycaps",
+    specs: [
+      "Cherry Profile",
+      "Material: PC",
+      "Support Full Keyboard Layouts",
+      "172 Keys",
+    ],
+    options: null,
+  },
+  {
+    id: "yuki-aim-keycap",
+    title: "Yuki Aim Keycap",
+    short:
+      "Yuki Aim Keycap compatible with all keyboards that using Original - Cherry Profile",
+    price: 15,
+    layout: "Cherry - Original Profile",
+    no_info: true,
+    available: false,
+    isNew: false,
+    lowStock: false,
+    images: [
+      "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285986918.jpg",
+      "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285992734.jpg",
+      "https://raw.githubusercontent.com/Glsswrks/hekeebcambodia/refs/heads/main/images/keycaps/tb_image_share_1766285996468.jpg",
+    ],
+    category: "keycaps",
+    specs: [
+      "Cherry Original Height Profile",
+      "Material: PBT Thermal transfer",
+      "Support 61/68 Layout Keyboards",
+      "104 Keys",
+    ],
+    options: null,
+  },
+];
